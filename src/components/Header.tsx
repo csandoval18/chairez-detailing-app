901c5442adf8d9279c754f2bef6b7a32d2693df4
csandoval18 <christiansandoval1018@outlook.com>
2024-09-02 22:17:23 -0500

    fixed some styling issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react"
+import { Link } from "react-scroll"
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
   const [openMenu, setOpenMenu] = useState(false)
+
   return (
     <header className="navbar container">
-      <a href="/" className="logo">
+      <Link
+        href="/"
+        className="logo"
+        smooth={true}
+        to="Hero"
+        onClick={() => setOpenMenu(false)}
+      >
         <img src="/logo-w.webp" alt="" width={100} height={60}></img>
-      </a>
+      </Link>
       <div
         onClick={() => {
           setOpenMenu(!openMenu)
@@ -18,10 +26,40 @@ const Header: React.FC<HeaderProps> = () => {
         }
       >
         <ul className="navMenu">
-          <li>link1</li>
-          <li>link2</li>
-          <li>link3</li>
-          <li>link4</li>
+          <li>
+            <Link
+              smooth={true}
+              to="Skills"
+              offset={-55}
+              onClick={() => setOpenMenu(false)}
+            ></Link>
+          </li>
+          <li>
+            <Link
+              smooth={true}
+              to="Skills"
+              offset={-55}
+              onClick={() => setOpenMenu(false)}
+            ></Link>
+          </li>
+          <li>
+            <Link
+              smooth={true}
+              to="Skills"
+              offset={-55}
+              onClick={() => setOpenMenu(false)}
+            ></Link>
+          </li>
+          <li>
+            <Link
+              smooth={true}
+              to="Contact"
+              offset={-55}
+              onClick={() => setOpenMenu(false)}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
       <div
